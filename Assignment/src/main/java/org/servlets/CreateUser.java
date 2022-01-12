dc151a3c0278d2a0548a240541b095bce3338638
antelligent07 <ewajaahat@gmail.com>
2022-01-12 23:15:09 +0530

    updated CreateUser servlet

--- Assignment/src/main/java/org/servlets/CreateUser.java
+++ Assignment/src/main/java/org/servlets/CreateUser.java
@@ -1,11 +1,16 @@
 package org.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.assignment.dao.UserDAO;
+import org.assignment.user.User;
 
 @WebServlet("/CreateUser")
 public class CreateUser extends HttpServlet {
@@ -16,8 +21,33 @@ public class CreateUser extends HttpServlet {
     }
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		int status = 0;
 		
 		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		String firstName = request.getParameter("firstName");
+		String lastName = request.getParameter("lastName");
+		String dateOfBirth = request.getParameter("dateOfBirth");
+		String city = request.getParameter("city");
+		String mobileNumber = request.getParameter("mobileNumber");
+		
+		
+		try {
+			status = UserDAO.createNewUser(new User(firstName, lastName, dateOfBirth, city, mobileNumber));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Status:" + status);
+		if (status > 0) {
+			out.println("<h3>User added !!!</h3>");
+			request.getRequestDispatcher("index.html").include(request, response);
+		}
+		else {
+			out.println("Failed to add user");
+		}
+		out.close();
 	}
-
 }
